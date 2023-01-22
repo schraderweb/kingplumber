4fd8e25e8b908520b0f092ea2a8f052c5f259073
yasir <yasiriqbal1990@gmail.com>
2023-01-23 03:28:25 +0500

    finished home page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
-function toggleMenu(){
+var swiper = new Swiper(".mySwiper", {
 
-    console.log("toggleMenu");
-}
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  });
+
+function openMenu(){
+
+    document.getElementById("toggleMenu").classList.remove("close");
+    document.getElementById("toggleMenu").classList.add("mobile-items");
+
+    document.getElementById("openMenu").classList.remove("open");
+    document.getElementById("openMenu").classList.add("close");
+
+    document.getElementById("closeMenu").classList.add("open");
+    document.getElementById("closeMenu").classList.remove("close");
+
+   
+      
+  }
+
+function closeMenu(){
+
+
+    document.getElementById("toggleMenu").classList.add("close");
+    document.getElementById("toggleMenu").classList.remove("mobile-items");
+    
+    document.getElementById("openMenu").classList.add("open");
+    document.getElementById("openMenu").classList.remove("close");
+
+    document.getElementById("closeMenu").classList.remove("open");
+    document.getElementById("closeMenu").classList.add("close");
+   
+      
+  }
